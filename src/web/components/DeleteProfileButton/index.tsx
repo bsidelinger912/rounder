673773fd22467084610629876dff6a15e5ac1ec3
Ben Sidelinger <ben_sidelinger@discovery.com>
2018-11-14 10:16:36 -0800

    rough out undo in delete profile toast

--- src/web/components/DeleteProfileButton/index.tsx
+++ src/web/components/DeleteProfileButton/index.tsx
@@ -26,7 +26,54 @@ const DeleteProfile = gql`
   }
 `;
 
+const RestoreProfile = gql`
+mutation RestoreProfile($id: ID!) {
+  restoreProfile(id: $id) {
+    id
+    name
+    description
+  }
+}
+`;
+
 class DeleteProfileMutation extends Mutation<IProfile, { id: string; }>{}
+class RestoreProfileMutation extends Mutation<IProfile, { id: string; }>{}
+
+const ProfileDeletedToast: React.SFC<Props> = ({ id }) => {
+  return (
+    <RestoreProfileMutation 
+      mutation={RestoreProfile} 
+      variables={{ id }}
+      update={(cache) => {
+        const { user } = cache.readQuery({ query: UserQuery }) as any;
+
+        const index = user.profiles.indexOf((profile: IProfile) => profile.id === id);
+        user.profiles.splice(index, 1);
+        
+        cache.writeQuery({
+          query: UserQuery,
+          data: { user }
+        })
+      }}
+    >
+      {(restoreProfile, { loading, error }) => {
+        if (error) {
+          console.error(error);
+          toast.error('there was an error restoring the profile');
+        }
+
+        const restoreMethod = () => {
+          if (loading) {
+            return;
+          }
+          restoreProfile({ variables: { id } });
+        };
+
+        return <span>The profile has been deleted.  <a onClick={restoreMethod}>undo</a></span>;
+      }}
+    </RestoreProfileMutation>
+  );
+};
 
 const DeleteProfileButton: React.SFC<Props> = ({ id }) => {
   return (
@@ -48,12 +95,12 @@ const DeleteProfileButton: React.SFC<Props> = ({ id }) => {
       {(deleteProfile, { loading, error }) => {
         if (error) {
           console.error(error);
-          alert('there was an error deleting the profile');
+          toast.error('there was an error deleting the profile');
         }
 
         const deleteMethod = () => {
           deleteProfile({ variables: { id } });
-          toast.warn('Profile Deleted');
+          toast(<ProfileDeletedToast id={id} />);
         };
         
         return <IconButton iconType={IconType.Trash} loading={loading} onClick={deleteMethod} />;
